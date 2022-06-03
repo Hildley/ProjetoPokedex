c97863f59fe8bb2426b617d97d0dc4d992903575
Hildley César <hilkiracesar@gmail.com>
2022-06-02 22:41:16 -0300

    Arquivos do projeto para avaliação

---
+++ index.js
@@ -0,0 +1,29 @@
+let message = "";
+
+const express = require("express");
+
+const path = require("path");
+
+
+const app = express();
+
+app.use(express.urlencoded());
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  setTimeout(() => {
+    message = "";
+  }, 1000);
+  res.render("index",{message});
+});
+
+app.post('/cadastra' , (req, res) =>{
+  const {nome, tipo, imagem} = req.body;
+  message = `Parabens, Voce cadastrou o pokemon ${nome} com sucesso`;
+  res.redirect("/", {nome: nome, tipo: tipo, imagem: imagem});
+})
+
+app.listen(3000)
