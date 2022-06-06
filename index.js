e9f80522e4722ee74e524b4bab5415b58bc063f3
Hildley César <hilkiracesar@gmail.com>
2022-06-06 20:46:04 -0300

    configuração para deploy

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("dotenv").config;
+
 let message = "";
 
 const express = require("express");
 
 const path = require("path");
 
-
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded());
 
